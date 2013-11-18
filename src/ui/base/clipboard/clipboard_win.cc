6768b609b6617488169136ba80530449b295f147
Calum Robinson <crobinson15@bloomberg.net>
2013-11-18 15:08:00 +0000

    GlobalLock the image data we read from the clipboard
    DRQS 46848155

--- src/ui/base/clipboard/clipboard_win.cc
+++ src/ui/base/clipboard/clipboard_win.cc
@@ -537,7 +537,11 @@ SkBitmap Clipboard::ReadImage(Buffer buffer) const {
   // We use a DIB rather than a DDB here since ::GetObject() with the
   // HBITMAP returned from ::GetClipboardData(CF_BITMAP) always reports a color
   // depth of 32bpp.
-  BITMAPINFO* bitmap = static_cast<BITMAPINFO*>(::GetClipboardData(CF_DIB));
+  HANDLE hBitmap = ::GetClipboardData(CF_DIB);
+  if (!hBitmap)
+    return SkBitmap();
+
+  BITMAPINFO* bitmap = static_cast<BITMAPINFO*>(GlobalLock(hBitmap));
   if (!bitmap)
     return SkBitmap();
   int color_table_length = 0;
@@ -592,6 +596,8 @@ SkBitmap Clipboard::ReadImage(Buffer buffer) const {
       MakeBitmapOpaque(device_bitmap);
     }
   }
+
+  GlobalUnlock(hBitmap);
 
   return canvas.ExtractImageRep().sk_bitmap();
 }

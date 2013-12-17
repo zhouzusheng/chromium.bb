526d2aba77a3e6b02bb8220063c42d848a5f38bf
Shezan Baig <sbaig1@bloomberg.net>
2013-12-17 14:36:24 -0500

    Send spellcheck messages only to processes with the same context
    This is:
      https://codereview.chromium.org/60383005/

--- src/chrome/browser/spellchecker/spellcheck_service.cc
+++ src/chrome/browser/spellchecker/spellcheck_service.cc
@@ -227,7 +227,10 @@ void SpellcheckService::OnCustomDictionaryChanged(
   for (content::RenderProcessHost::iterator i(
           content::RenderProcessHost::AllHostsIterator());
        !i.IsAtEnd(); i.Advance()) {
-    i.GetCurrentValue()->Send(new SpellCheckMsg_CustomDictionaryChanged(
+    content::RenderProcessHost* process = i.GetCurrentValue();
+    if (!process || context_ != process->GetBrowserContext())
+      continue;
+    process->Send(new SpellCheckMsg_CustomDictionaryChanged(
         dictionary_change.to_add(),
         dictionary_change.to_remove()));
   }
@@ -277,6 +280,8 @@ void SpellcheckService::OnEnableAutoSpellCorrectChanged() {
            content::RenderProcessHost::AllHostsIterator());
        !i.IsAtEnd(); i.Advance()) {
     content::RenderProcessHost* process = i.GetCurrentValue();
+    if (!process || context_ != process->GetBrowserContext())
+      continue;
     process->Send(new SpellCheckMsg_EnableAutoSpellCorrect(enabled));
   }
 }

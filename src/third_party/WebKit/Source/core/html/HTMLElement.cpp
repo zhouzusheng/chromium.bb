087f958267336ff2f1691276727f74b6699e798e
Shezan Baig <sbaig1@bloomberg.net>
2014-02-26 10:48:22 -0500

    Fix compile error for v30
    * Use ExceptionState instead of ExceptionCode

--- src/third_party/WebKit/Source/core/html/HTMLElement.cpp
+++ src/third_party/WebKit/Source/core/html/HTMLElement.cpp
@@ -305,9 +305,9 @@ void HTMLElement::parseAttribute(const QualifiedName& name, const AtomicString& 
             if (value.isNull() || equalIgnoringCase(value, "false")) {
                 RefPtr<Range> range = Range::create(document());
 
-                ExceptionCode ec = 0;
-                range->selectNode(this, ec);
-                if (ec == 0)
+                TrackExceptionState es;
+                range->selectNode(this, es);
+                if (!es.hadException())
                     document()->frame()->editor()->clearMisspellingsAndBadGrammar(VisibleSelection(range.get()));
             }
         }

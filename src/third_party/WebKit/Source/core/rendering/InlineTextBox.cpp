8c60d84b3a0f78a189c39ac1bba251b791347a7f
Shezan Baig <sbaig1@bloomberg.net>
2015-01-30 15:50:22 -0500

    Fix compile errors for v38
    * InlineTextBox::textRenderer has been renamed to InlineTextBox::renderer
      (https://codereview.chromium.org/414863002)

--- src/third_party/WebKit/Source/core/rendering/InlineTextBox.cpp
+++ src/third_party/WebKit/Source/core/rendering/InlineTextBox.cpp
@@ -1223,8 +1223,8 @@ void InlineTextBox::paintDocumentMarker(GraphicsContext* pt, const FloatPoint& b
     }
 
     Color markerColor(255,0,0,255);
-    if (textRenderer().node()) {
-        const Element *element = textRenderer().node()->rootEditableElement();
+    if (renderer().node()) {
+        const Element *element = renderer().node()->rootEditableElement();
         if (element && element->hasAttributes()) {
             AtomicString colorAttr = nullAtom;
 

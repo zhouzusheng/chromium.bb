d4496c47073a10d65f32d814d972d9e41ffaae79
Shezan Baig <sbaig1@bloomberg.net>
2015-02-02 14:56:24 -0500

    Move blink's inspector resources to a separate grd file
    This allows us to repack it into our devtools pak file.

--- src/content/child/blink_platform_impl.cc
+++ src/content/child/blink_platform_impl.cc
@@ -27,6 +27,7 @@
 #include "base/sys_info.h"
 #include "base/time/time.h"
 #include "blink/public/resources/grit/blink_resources.h"
+#include "blink/public/resources/grit/blink_inspector_resources.h"
 #include "content/app/strings/grit/content_strings.h"
 #include "content/child/child_thread.h"
 #include "content/child/content_child_helpers.h"

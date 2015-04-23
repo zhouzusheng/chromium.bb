0f44abddd663f4e7bd70a7aa7245e99dc24431aa
Shezan Baig <sbaig1@bloomberg.net>
2015-04-23 16:56:03 -0400

    Don't adjust the window bounds for DPI settings
    The implementation of Widget::SetBounds adjusts the given rectangle by the
    screen's DPI.  This makes the WebView position not what the application expects
    it to be.
    
    There is a comment in DesktopNativeWidgetAura::SetBounds that indicates that
    this scaling logic should be removed.  For now though, just expose another
    method that explicitly doesn't do this scaling.  We can revisit this later to
    see if this scaling logic eventually gets removed from SetBounds upstream.

--- src/ui/views/widget/desktop_aura/desktop_native_widget_aura.cc
+++ src/ui/views/widget/desktop_aura/desktop_native_widget_aura.cc
@@ -708,6 +708,11 @@ void DesktopNativeWidgetAura::SetBounds(const gfx::Rect& bounds) {
   desktop_window_tree_host_->AsWindowTreeHost()->SetBounds(bounds_in_pixels);
 }
 
+void DesktopNativeWidgetAura::SetBoundsNoDPIAdjustment(const gfx::Rect& bounds) {
+  if (content_window_)
+    desktop_window_tree_host_->AsWindowTreeHost()->SetBounds(bounds);
+}
+
 void DesktopNativeWidgetAura::SetSize(const gfx::Size& size) {
   if (content_window_)
     desktop_window_tree_host_->SetSize(size);

f5df926d8fd345a503205812df424fe5d73f91f8
Daniel Wilkey <dwilkey2@bloomberg.net>
2015-06-10 16:22:09 -0400

    always activate the selection when finding ends, rather than remove it

--- src/blpwtk2/private/blpwtk2_webviewimpl.cc
+++ src/blpwtk2/private/blpwtk2_webviewimpl.cc
@@ -202,7 +202,7 @@ void WebViewImpl::handleFindRequest(const FindOnPageRequest& request)
     DCHECK(!d_wasDestroyed);
 
     if (!request.reqId) {
-        d_webContents->StopFinding(content::STOP_FIND_ACTION_CLEAR_SELECTION);
+        d_webContents->StopFinding(content::STOP_FIND_ACTION_ACTIVATE_SELECTION);
         return;
     }
     blink::WebFindOptions options;

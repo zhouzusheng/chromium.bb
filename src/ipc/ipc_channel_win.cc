8b659a3f822e1c62870633178ceac5c6aa6feeb0
Imran Haider <ihaider3@bloomberg.net>
2015-10-30 10:54:05 -0400

    Add traces to possible callers of OnChannelError
    This will help us better understand the source of the "channel error"
    message.

--- src/ipc/ipc_channel_win.cc
+++ src/ipc/ipc_channel_win.cc
@@ -233,6 +233,7 @@ void ChannelWin::HandleInternalMessage(const Message& msg) {
   if (failed) {
     NOTREACHED();
     Close();
+    LOG(WARNING) << "ChannelWin::HandleInternalMessage(): Calling OnChannelError()";
     listener()->OnChannelError();
     return;
   }
@@ -542,6 +543,7 @@ void ChannelWin::OnIOCompleted(
   if (!ok && pipe_.IsValid()) {
     // We don't want to re-enter Close().
     Close();
+    LOG(WARNING) << "ChannelWin::OnIOCompleted(): Calling OnChannelError()";
     listener()->OnChannelError();
   }
 }

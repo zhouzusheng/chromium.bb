47db5d52813469a7b76d88647df4c8044cba4b22
zhouzusheng <zhouzusheng@hylanda.com>
2016-11-28 18:17:51 +0800

    增加 depot_tools 目录
    增加 depot_tools 目录

---
+++ setenv.bat
@@ -0,0 +1,9 @@
+PATH=%~dp0depot_tools;%PATH%
+SET GYP_DEFINES=proprietary_codecs=1 ffmpeg_branding=Chrome component=static_library windows_sdk_path="C:\Program Files (x86)\Windows Kits\8.1"
+SET GYP_MSVS_VERSION=2013
+SET DEPOT_TOOLS_WIN_TOOLCHAIN=0
+SET GYP_GENERATORS=msvs-ninja,ninja
+SET HOME=%USERPROFILE%
+
+
+
